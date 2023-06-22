89d3ec6ab1949b3984b8625b89b5d4e1983ad108
abkolbeko <sasha-kolbeko@mail.ru>
2023-06-23 01:38:13 +0500

    list on war pointers and shared

--- C++/dummies/problems/add_two_num_2_list/add_two_num_2_list_shared.cpp
+++ C++/dummies/problems/add_two_num_2_list/add_two_num_2_list_shared.cpp
@@ -3,7 +3,7 @@
 
 struct ListNode {
     int val;
-    ListNode *next;
+    std::shared_ptr< ListNode > next;
     ListNode() : val(0), next(nullptr) {}
     ListNode(int x) : val(x), next(nullptr) {}
     ListNode(int x, ListNode *next) : val(x), next(next) {}
@@ -11,9 +11,9 @@ struct ListNode {
 
 class Solution {
 public:
-    ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) {
-        ListNode* tmp = nullptr, *res_start = nullptr;
-        ListNode* sum_node = new ListNode( 0 );
+    std::shared_ptr< ListNode > addTwoNumbers(std::shared_ptr< ListNode > l1, std::shared_ptr< ListNode > l2) {
+        std::shared_ptr< ListNode > tmp = nullptr, res_start = nullptr;
+        auto sum_node = std::shared_ptr< ListNode >( new ListNode( 0 ) );
         res_start = sum_node;
 
         while( l1 || l2 )
@@ -38,10 +38,10 @@ public:
             if( sum_node->val > 9 )
             {
                 sum_node->val -= 10;
-                sum_node->next = new ListNode( 1 );
+                sum_node->next = std::shared_ptr< ListNode >( new ListNode( 1 ) );
             }
             else if( l1 || l2 )
-                sum_node->next = new ListNode( 0 );
+                sum_node->next = std::shared_ptr< ListNode >( new ListNode( 0 ) );
             else
                 break;
             sum_node = sum_node->next;
@@ -52,15 +52,15 @@ public:
 
 int main()
 {
-    ListNode* n1 = nullptr, *n2 = nullptr;
+    std::shared_ptr< ListNode > n1 = nullptr, n2 = nullptr;
     Solution solver = Solution();
-    n1 = new ListNode( 2 );
-    n1->next = new ListNode( 4 );
-    n1->next->next = new ListNode( 3 );
-    n2 = new ListNode( 5 );
-    n2->next = new ListNode( 6 );
-    n2->next->next = new ListNode( 4 );
-    auto* result = solver.addTwoNumbers( n1, n2 );
+    n1 = std::shared_ptr< ListNode >( new ListNode( 2 ) );
+    n1->next = std::make_shared< ListNode >( ( 4 ) );
+    n1->next->next = std::make_shared< ListNode >( ( 3 ) );
+    n2 = std::shared_ptr< ListNode >( new ListNode( 5 ) );
+    n2->next = std::shared_ptr< ListNode >( new ListNode( 6 ) );
+    n2->next->next = std::shared_ptr< ListNode >( new ListNode( 4 ) );
+    auto result = solver.addTwoNumbers( n1, n2 );
     while( result )
     {
         std::cout<< result->val << " ";
