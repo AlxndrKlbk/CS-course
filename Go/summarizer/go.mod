cb73d33769905ced94b7b12c39403468c75f7912
abkolbeko <sasha-kolbeko@mail.ru>
2023-08-24 23:15:48 +0500

    summ from input

---
+++ Go/summarizer/go.mod
@@ -0,0 +1,3 @@
+module summarizer
+
+go 1.21.0
---
+++ Go/summarizer/summarizer.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+)
+
+func main() {
+	in := bufio.NewReader(os.Stdin)
+	out := bufio.NewWriter(os.Stdout)
+	defer out.Flush()
+
+	var testCount int
+	fmt.Fscan(in, &testCount)
+
+	for i := 0; i < testCount; i++ {
+		var n, m int
+		fmt.Fscan(in, &n, &m)
+		fmt.Fprintln(out, n+m)
+	}
+}
