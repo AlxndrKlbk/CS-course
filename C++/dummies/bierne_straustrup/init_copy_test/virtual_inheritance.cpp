9670f57e1131d0d0c4053fd28ea522b5186e3154
abkolbeko <sasha-kolbeko@mail.ru>
2024-08-01 18:20:05 +0500

    another tests with virtual methods

--- C++/dummies/bierne_straustrup/init_copy_test/virtual_inheritance.cpp
+++ C++/dummies/bierne_straustrup/init_copy_test/virtual_inheritance.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <memory>
 
 class BaseWithoutVirtual {
 public:
@@ -15,11 +16,11 @@ class Base {
 public:
     Base() { print(); }
 
-    virtual ~Base() { print(); }
+    virtual ~Base() { std::cout << "destruction: "; print(); }
 
     // void print() { std::cout << " Base print"<< std::endl; }    //1
 
-    virtual void print() { std::cout << " Base print"<< std::endl; } //2
+    virtual void print()  { std::cout << " Base print"<< std::endl; } //2
 
     void basePrint() { print(); }
 };
@@ -28,21 +29,32 @@ class Derived : public Base {
 public:
     Derived() { print(); }
 
-    ~Derived() { print(); }
+    ~Derived() { std::cout << "destruction: " ; print(); }
 
     // virtual void print() { std::cout << " Derived print" << std::endl; }    //1
 
     virtual void print() override { std::cout << " Derived print" << std::endl; }    //2
+
+    // virtual void print() override { throw 1; std::cout << " Derived print" << std::endl; }    //3
+
 };
 
 static void test() {
-    std::cout << "Test Derived and Base with virtual methods" << std::endl;
-    Base * ptr = new Derived;
+    std::cout << std::endl << "Test Derived and Base with virtual methods" << std::endl;
 
+    Base* ptr = new Derived;
     ptr->basePrint();
-
     delete ptr;
 
+    // try {
+    //     auto ptr = std::make_shared<Derived>();
+
+    //     ptr->basePrint();
+
+    //     // delete ptr;
+    // }
+    // catch   (...) {
+    // }    // 3
     std::cout << "BaseWithoutVirtual: " << sizeof(BaseWithoutVirtual) << std::endl;
     std::cout << "Base: " << sizeof(Base) << std::endl;
     std::cout << "Derived: " << sizeof(Derived) << std::endl;
