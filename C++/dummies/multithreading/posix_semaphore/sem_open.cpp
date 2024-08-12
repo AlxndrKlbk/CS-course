7ee88343457753c15642d7674ef644f3c1ba40e1
abkolbeko <sasha-kolbeko@mail.ru>
2024-08-12 21:05:29 +0500

    small semaphore usage

---
+++ C++/dummies/multithreading/posix_semaphore/sem_open.cpp
@@ -0,0 +1,38 @@
+#include <fcntl.h>
+#include <sys/stat.h>
+#include <semaphore.h>
+#include <stdio.h>
+
+#define SEMAPHORE_NAME "/my_named_semaphore"
+
+int main(int argc, char ** argv) {
+    sem_t *sem;
+
+    if ( argc == 2 ) {
+        printf("Dropping semaphore...\n");
+        if ( (sem = sem_open(SEMAPHORE_NAME, 0)) == SEM_FAILED ) {
+            perror("sem_open");
+            return 1;
+        }
+        sem_post(sem);
+        perror("sem_post");
+        printf("Semaphore dropped.\n");
+        return 0;
+    }
+
+    if ( (sem = sem_open(SEMAPHORE_NAME, O_CREAT, 0777, 0)) == SEM_FAILED ) {
+        perror("sem_open");
+        return 1;
+    }
+
+    printf("Semaphore is taken.\nWaiting for it to be dropped.\n");
+    auto sem_flag = sem_wait(sem);
+    if ( sem_flag < 0 )
+        perror("sem_wait");
+
+    sem_flag = sem_close(sem);
+    if ( sem_flag < 0 )
+        perror("sem_close");
+
+    return 0;
+}
