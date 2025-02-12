9d38027a93a6877153a8827b1b66162b55533c41
abkolbeko <sasha-kolbeko@mail.ru>
2025-02-12 09:52:40 +0500

    Donald Knuth solution for find the duplicate number (O(n) time and O(1) space complexity)

---
+++ C++/dummies/problems/find_the_duplicate_number/solution.cpp
@@ -0,0 +1,39 @@
+#include <vector>
+
+#include <cassert>
+
+class Solution {
+public:
+
+    // Donuld Knutt solve it for one day
+    int findDuplicate(std::vector<int>& nums) {
+        int slow = nums[0];
+        int fast = nums[0];
+
+        // Find the intersection point of the two pointers
+        do {
+            slow = nums[slow];
+            fast = nums[nums[fast]];
+        } while (slow != fast);
+
+        // Find the entrance of the cycle
+        slow = nums[0];
+        while (slow != fast) {
+            slow = nums[slow];
+            fast = nums[fast];
+        }
+
+        return slow;
+    }
+};
+
+
+int main (int argc, char** argv)
+{
+    std::vector<int> nums{1, 2, 3, 444, 444, 4};
+    Solution solution;
+    auto const& res = solution.findDuplicate(nums);
+    assert(res == 444);
+
+    return 0;
+}
