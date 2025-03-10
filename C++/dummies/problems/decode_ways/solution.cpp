f5b484ae031c2310597d9cd8efa18b49440c1db3
abkolbeko <sasha-kolbeko@mail.ru>
2025-03-10 09:49:40 +0500

    decode ways problem

---
+++ C++/dummies/problems/decode_ways/solution.cpp
@@ -0,0 +1,41 @@
+#include <string>
+#include <sys/types.h>
+#include <vector>
+#include <cassert>
+
+class Solution {
+public:
+    int numDecodings(std::string s) {
+        if (s[0] == '0') {
+            return 0;
+        }
+
+        int n = s.length();
+        std::vector<int> dp(n + 1, 0);
+        dp[0] = dp[1] = 1;
+
+        for (int i = 2; i <= n; i++) {
+            int one = s[i - 1] - '0';
+            int two = stoi(s.substr(i - 2, 2));
+
+            if (1 <= one && one <= 9) {
+                dp[i] += dp[i - 1];
+            }
+            if (10 <= two && two <= 26) {
+                dp[i] += dp[i - 2];
+            }
+        }
+
+        return dp[n];
+    }
+};
+
+int main (int argc, char** argv)
+{
+    std::vector<int> nums{1,2,3};
+    Solution solution;
+    auto res = solution.numDecodings("226");
+    assert(res == 3);
+
+    return 0;
+}
