0cb5fdc66744fce21c175690bd0c49d7e8621a89
abkolbeko <sasha-kolbeko@mail.ru>
2025-03-13 11:05:23 +0500

    tests with virtual inheritance and call constructors

---
+++ C++/dummies/bierne_straustrup/standard_explore/virtual_inheritance/source.cpp
@@ -0,0 +1,52 @@
+#include <iostream>
+
+class PoweredDevice {
+public:
+    PoweredDevice(int power) : mValue(power) {
+        std::cout << "PoweredDevice: " << power << '\n';
+    }
+
+    virtual int get_value() { return mValue; }
+private:
+    int mValue;
+};
+
+class Scanner : virtual public PoweredDevice {
+public:
+    Scanner(int scanner, int power) : PoweredDevice(power), mValue(power) {
+        std::cout << "Scanner: " << scanner << '\n';
+    }
+
+    int get_value() override { return mValue; }
+
+private:
+    int mValue;
+};
+
+class Printer : virtual public PoweredDevice {
+public:
+    Printer(int printer, int power) : PoweredDevice(power), mValue(power) {
+        std::cout << "Printer: " << printer << '\n';
+    }
+
+    int get_value() override { return mValue; }
+
+private:
+    int mValue;
+};
+
+class Copier : public Scanner, public Printer {
+public:
+    Copier(int scanner, int printer, int power) : PoweredDevice(power), Scanner(scanner, scanner), Printer(printer, printer) {
+        std::cout << "Copier: " << '\n';
+    }
+
+    int get_value() override { return Printer::get_value(); }
+
+};
+
+int main() {
+    Copier copier(1, 2, 100);
+    std::cout << copier.get_value() << std::endl;
+    return 0;
+}
